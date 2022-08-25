4d04e409f0b0314568462388b58bce0005e3c285
joelngyx <joelngyx@gmail.com>
2022-08-25 23:59:05 +0800

    Removed more warnings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import LandingPage from './pages/landing-page/LandingPage.js';
-import { Helmet } from 'react-helmet';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import AnimatedRoutes from './components/AnimatedRoutes';
 
 function App() {
